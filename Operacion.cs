dcc6f0448cad8d1540354eb4e62ad6aa1bc3b66c
mtsluna <mtsluna@gmail.com>
2020-09-23 23:57:51 -0300

    Agregar archivos de proyecto.

---
+++ Operacion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2desafio3
+{
+    class Operacion
+    {
+        public static int suma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int resta(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2desafio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bienvenido");
+
+            bool isValid = true;
+            while (isValid)
+            {
+                try
+                {
+                    Console.Write("Ingrese un número 'a': ");
+                    int a = Int16.Parse(Console.ReadLine());
+                    Console.Write("Ingrese un número 'b': ");
+                    int b = Int16.Parse(Console.ReadLine());
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine("La suma entre 'a' y 'b' es: " + Operacion.suma(a, b));
+                    Console.WriteLine("La resta entre 'a' y 'b' es: " + Operacion.resta(a, b));
+                    Console.WriteLine("--------------------------------------------");                    
+                    isValid = false;
+                }
+                catch (Exception e)
+                {
+                    isValid = true;
+                    Console.WriteLine("Uno de los números ingresados es incorrecto.");                    
+                    Console.WriteLine("Vuelva a ingresarlos:");
+                }
+            }
+            Console.Write("Pulse una tecla para finalizar...");
+            Console.ReadKey();
+        }
+    }
+}
